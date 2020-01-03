acb4978fa8025ffbb659c0fbc2c4e6f9ffcbda00
Sourabh1924 <memo.sourabh@gmail.com>
2020-01-03 18:43:00 +0530

    add

--- SalesForce/src/com/SaleForce/batfile/CreateContracts.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\iDeliver20\Desktop\Selenium_Workspace\Saleforce\SalesForce
-cd %projectLocation%
-set classpath=%projectLocation%\bin;C:\Java_jar\*
-java org.testng.TestNG %projectLocation%\Contracts.xml
-pause
--- SalesForce/src/com/SaleForce/batfile/CreateLeads.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=SalesForce/SalesForce/
-cd %projectLocation%
-set classpath=%projectLocation%\bin;C:\Java_jar\*
-java org.testng.TestNG %projectLocation%\Leads.xml
-pause
